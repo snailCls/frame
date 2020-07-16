e0251e26b0075783ad27d1ac2f983cb443ef8717
snail <1207408091@qq.com>
2020-07-16 18:13:50 +0800

    first commit: 拦截器

---
+++ realize/src/main/java/zzh/realize/config/MyWebConfigurer.java
@@ -0,0 +1,13 @@
+package zzh.realize.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ * webMvc配置器
+ */
+public class MyWebConfigurer implements WebMvcConfigurer {
+
+
+
+}
