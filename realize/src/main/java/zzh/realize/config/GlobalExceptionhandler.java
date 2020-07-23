e371f98776a3689430307f817117b6c1720ed72e
snail <1207408091@qq.com>
2020-07-23 14:15:20 +0800

    2020/07/23 全局异常拦截器修改啊

--- realize/src/main/java/zzh/realize/config/GlobalExceptionhandler.java
+++ realize/src/main/java/zzh/realize/config/GlobalExceptionhandler.java
@@ -1,38 +1,36 @@
 package zzh.realize.config;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.env.MissingRequiredPropertiesException;
-import org.springframework.validation.BindException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import zzh.realize.result.JsonResponseResult;
+import org.springframework.validation.BindException;
 
-
+/**
+ * 全局异常报错信息处理器
+ * @author snail
+ */
 @ControllerAdvice
-public class GlobalExceptionhandler {
+public class GlobalExceptionHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionhandler.class);
+    private static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    public @ResponseBody String jsonErrorHandler(Exception e) throws Exception {
-        logger.error("异常信息 : ", e);
-        if(e instanceof BindException) {
+    public @ResponseBody JsonResponseResult jsonErrorHandler(Exception e) throws Exception {
+        logger.error("异常信息 : " , e);
+        if (e instanceof BindException) {
             BindException be = (BindException) e;
-            if(be.getErrorCount() > 0) {
-                return new String("请求地址不存在");
-            }else if(e instanceof HttpRequestMethodNotSupportedException) {
-                return new String("不支持[" + ((HttpRequestMethodNotSupportedException) e).getMethod() + "]");
-            }else if(e instanceof MissingServletRequestParameterException) {
-                return new String("参数[" + ((MissingServletRequestParameterException) e).getParameterName() + "]不能为空");
-            }else if(e instanceof MethodArgumentTypeMismatchException) {
-                String exName = ((MethodArgumentTypeMismatchException) e).getRequiredType().getName();
-                return new String("参数[" + ((MethodArgumentTypeMismatchException) e).getName() + "]类型转换出错," +
-                        "需要类型为" + exName.substring(exName.lastIndexOf(".") + 1) + ",值为 : " + ((MethodArgumentTypeMismatchException) e).getValue());
+            if(be.getErrorCount() > 0 ) {
+                return JsonResponseResult.errorResult(be.getBindingResult().getFieldError().getDefaultMessage());
+            } else if(e instanceof NoHandlerFoundException) {
+                return JsonResponseResult.errorResult("请求的地址不存在");
+            }else {
+                return JsonResponseResult.errorResult("系统繁忙，请稍后再试");
             }
         }
-        return new String("系统繁忙，请稍后再试");
+        return JsonResponseResult.errorResult("系统繁忙，请稍后再试");
     }
 
 }
